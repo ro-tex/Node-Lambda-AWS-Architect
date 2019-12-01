2fa54e6330e3250d159e3faab9c313d67b79da99
Ivaylo Novakov <inovakov@gmail.com>
2019-12-01 15:40:44 +0100

    License, repo link, commands.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const { Authorizer, RequestLogger, PlatformClient } = require('microservice-util
 
 let logger = new RequestLogger();
 const api = new Api({
-	requestMiddleware(request) {
+  requestMiddleware(request) {
 		logger.log({ title: 'RequestLogger', level: 'INFO', request: request });
-		let userToken = request.requestContext.authorizer && request.requestContext.authorizer.jwt;
-		request.userPlatformClient = new PlatformClient(msg => logger.log(msg), () => userToken);
-		return request;
-	}
+    let userToken = request.requestContext.authorizer && request.requestContext.authorizer.jwt;
+    request.userPlatformClient = new PlatformClient(msg => logger.log(msg), () => userToken);
+    return request;
+  }
 });
 module.exports = api;
 
@@ -22,12 +22,12 @@ api.onEvent(trigger => {});
 api.onSchedule(trigger => {});
 
 api.setAuthorizer(request => {
-	return authorizer.getPolicy(request);
+  return authorizer.getPolicy(request);
 });
 
 api.get('/.well-known/openapi.json', async () => {
-	let openapiFile = path.join(__dirname, './openapi.json');
-	let data = await fs.readJson(openapiFile);
+  let openapiFile = path.join(__dirname, './openapi.json');
+  let data = await fs.readJson(openapiFile);
 	return { statusCode: 200, body: data };
 });
 
@@ -40,49 +40,49 @@ api.get('/v1/resource/{resourceId}', request => {
 });
 
 api.options('/{proxy+}', request => {
-	return {
-		statusCode: 200,
-		headers: {
-			'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key',
-			'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
-			'Access-Control-Allow-Origin': request.headers.Origin || '*'
-		}
-	};
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key',
+      'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
+      'Access-Control-Allow-Origin': request.headers.Origin || '*'
+    }
+  };
 });
 
 api.any('/{proxy+}', request => {
-	/*
-		{
-			request: {
-				"resource": "/{proxy+}",
-				"path": "/a/b/c",
-				"httpMethod": "GET",
-				"headers": {
-					"Content-type": " application/json"
-				},
-				"queryStringParameters": {
-					"param1": "1"
-				},
-				"pathParameters": {
-					"resourceId": "123"
-				},
-				"stageVariables": null,
-				"requestContext": {
-					"authorizer": {
-						"principalIdId": "USER-TOKEN-SUB"
-					},
-					"accountId": "aws",
-					"resourceId": "wagagr",
-					"stage": "test-invoke-stage",
-					"requestId": "test-invoke-request",
-					"identity": {...},
-					"resourcePath": "/{proxy+}",
-					"httpMethod": "GET",
-					"apiId": "apiId"
-				},
-				"body": null
-			}
-		}
-	*/
+  /*
+  	{
+  		request: {
+  			"resource": "/{proxy+}",
+  			"path": "/a/b/c",
+  			"httpMethod": "GET",
+  			"headers": {
+  				"Content-type": " application/json"
+  			},
+  			"queryStringParameters": {
+  				"param1": "1"
+  			},
+  			"pathParameters": {
+  				"resourceId": "123"
+  			},
+  			"stageVariables": null,
+  			"requestContext": {
+  				"authorizer": {
+  					"principalIdId": "USER-TOKEN-SUB"
+  				},
+  				"accountId": "aws",
+  				"resourceId": "wagagr",
+  				"stage": "test-invoke-stage",
+  				"requestId": "test-invoke-request",
+  				"identity": {...},
+  				"resourcePath": "/{proxy+}",
+  				"httpMethod": "GET",
+  				"apiId": "apiId"
+  			},
+  			"body": null
+  		}
+  	}
+  */
 	return { statusCode: 404 };
 });
